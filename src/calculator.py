9145d7f88e09f6c9344aeb8831dc1a4c0e9eba6f
Jorge Orozco <yorchorozco@gmail.com>
2019-04-15 18:30:32 +0200

    intial commit for QA

---
+++ src/calculator.py
@@ -0,0 +1,24 @@
+import math
+
+
+
+
+# sum function should go here
+
+def suma(a,b):
+    pass
+
+# substraction function should go here 
+
+def subs(a,b):
+    pass
+
+
+
+# division function should go here
+def divis(a,b):
+    pass
+
+# multiplication function should go here
+def multip(a,b):
+    pass
